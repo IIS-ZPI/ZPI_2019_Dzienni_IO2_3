592e2708d7242069d0c9c5c1b44c0537d35a8ab2
Piotr Plecinski <209432@edu.p.lodz.pl>
2019-05-15 15:44:19 +0200

    zadanie2 - dodanie swojego github id do wyswietlania

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Grupa: ZPI_2019_Dzienni_IO2_3, Rola: Developer, Team Leader: 209347");
+        System.out.println("piotrk281097");
     }
 
 }
