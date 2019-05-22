d6af6959c3deb6847121dde75820722dca7526a3
nciosek <n_ciosek@wp.pl>
2019-05-22 13:45:22 +0200

    zadanie 3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Grupa: ZPI_2019_Dzienni_IO2_3, Rola: Developer, Team Leader: 209347");
+        System.out.println("Grupa: ZPI_2019_Dzienni_IO2_3, Rola: operations, Team Leader: nciosek");
         System.out.println("nciosek");
     }
 
