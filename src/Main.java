86dba32e7c388bd8ada611636d3119bcf9824b5d
nciosek <n_ciosek@wp.pl>
2019-05-22 14:18:43 +0200

    zadanie 4 - interfejs dodawania i jego implementacja

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
-public class Main{
+public class Main implements IArithmeticsAdd{
 
     public static void main(String[] args) {
 
         System.out.println("Grupa: ZPI_2019_Dzienni_IO2_3, Rola: operations, Team Leader: nciosek");
         System.out.println("nciosek");
+    }
+
+    @Override
+    public double Addition(double A, double B) {
+        return A+B;
     }
 
 
