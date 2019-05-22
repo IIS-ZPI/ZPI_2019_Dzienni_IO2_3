de25d9177f4af5cd1f439385b79a7773f09ce7c3
Michal <209333@edu.p.lodz.pl>
2019-05-22 14:04:11 +0200

    Zadanie 4 - interfejs odejmowania i implementacja metody

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
-public class Main {
+
+public class Main implements IArithmeticsDiv {
 
     public static void main(String[] args) {
         System.out.println("Grupa: ZPI_2019_Dzienni_IO2_3, Rola: Developer, Team Leader: 209347");
         System.out.println("Bigoz005");
     }
 
+
+    @Override
+    public double Division(double A, double B) {
+        double C;
+        if (B != 0) {
+            C = A / B;
+            return C;
+        }else {
+            System.out.println("B cannot be 0");
+            return 0;
+        }
+    }
 }
