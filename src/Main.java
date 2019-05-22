3f42f27aff705af34ae7ca7416f5a047d0813211
Piotr Plecinski <209432@edu.p.lodz.pl>
2019-05-22 15:19:36 +0200

    zad4

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
-public class Main {
+public class Main implements IArithmeticsMult{
 
     public static void main(String[] args) {
 
         System.out.println("Grupa: ZPI_2019_Dzienni_IO2_3, Rola: Developer, Team Leader: 209347");
         System.out.println("piotrek281097");
+    }
+
+    @Override
+    double Multiplication(double A, double B) {
+        return A * B;
     }
 
 }
