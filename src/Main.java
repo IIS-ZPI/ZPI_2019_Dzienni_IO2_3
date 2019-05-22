fbeffe99af4aa363d61a19dec2607ac88004c77c
nciosek <n_ciosek@wp.pl>
2019-05-22 13:35:45 +0200

    zadanie 2 - wyswietlenie gitid

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Grupa: ZPI_2019_Dzienni_IO2_3, Rola: Developer, Team Leader: 209347");
-    		System.out.println("nciosek");
-        System.out.println("Bigoz005");
-
+        System.out.println("nciosek");
     }
 
 }
