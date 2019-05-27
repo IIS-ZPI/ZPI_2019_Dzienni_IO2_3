5b7899c2e3306ab544c28c1fcd38589ce21ef1df
Piotr Plecinski <209432@edu.p.lodz.pl>
2019-05-27 13:07:51 +0200

    zad5 branch piotr22

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,7 @@ public class Main implements IArithmeticsDiff, IArithmeticsMult, IArithmeticsAdd
     public double Difference(double A, double B) {
         return A - B;
     }
-
+    //komentarz zad5 piotr22
     @Override
     public double Multiplication(double A, double B) {
         return A * B;
