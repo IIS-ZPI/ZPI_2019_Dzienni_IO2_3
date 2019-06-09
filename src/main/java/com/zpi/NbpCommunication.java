c3441a5b9ee5f963259204e0f710a2fc9b16bbe8
MichałNawrot <michal.nawrot.mn.mn@gmail.com>
2019-06-09 20:37:41 +0200

    Pobranie Tabeli C z serwera NBP

--- src/main/java/com/zpi/NbpCommunication.java
+++ src/main/java/com/zpi/NbpCommunication.java
@@ -2,6 +2,7 @@ package com.zpi;
 
 import com.google.gson.Gson;
 import com.zpi.data.NbpTableB;
+import com.zpi.data.NbpTableC;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
@@ -24,5 +25,17 @@ public class NbpCommunication {
         ArrayList<NbpTableB> nbpTableB = new ArrayList<>(Arrays.asList(gson.fromJson(result, NbpTableB[].class)));
         System.out.println(nbpTableB.get(0).getRates().get(0).getCode() + " " + nbpTableB.get(0).getRates().get(0).getMid());
         return nbpTableB;
+    }
+
+    private static List<NbpTableC> getNbpTableC()
+    {
+        final String uri = "http://api.nbp.pl/api/exchangerates/tables/C/";
+
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(uri, String.class);
+        Gson gson = new Gson();
+        ArrayList<NbpTableC> nbpTableC = new ArrayList<>(Arrays.asList(gson.fromJson(result, NbpTableC[].class)));
+        System.out.println(nbpTableC.get(0).getRates().get(0).getCode() + " " + nbpTableC.get(0).getRates().get(0).getMid());
+        return nbpTableC;
     }
 }
