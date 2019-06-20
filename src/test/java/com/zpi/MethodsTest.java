fe14960149340bbc0ecd7c3f24edda34df570fc5
Piotr Plecinski <209432@edu.p.lodz.pl>
2019-06-20 15:57:44 +0200

    dodanie funkcji do analizy sesji oraz dodanie i poprawa testów

--- src/test/java/com/zpi/MethodsTest.java
+++ src/test/java/com/zpi/MethodsTest.java
@@ -8,47 +8,47 @@ import static org.junit.Assert.*;
 public class MethodsTest {
 
     @Test
-    public void findMeanShouldReturn49coma2ForArrayTest() {
-        double[] arrayTest = {44, 50, 38, 96, 42, 47, 40, 39, 46, 50};
+    public void findMeanShouldReturnMeanValueArrayTest1() {
+        double[] arrayTest1 = {44, 50, 38, 96, 42, 47, 40, 39, 46, 50};
 
-        assertThat(Methods.findMean(arrayTest), is(49.2));
+        assertThat(Methods.findMean(arrayTest1), is(49.2));
     }
 
     @Test
-    public void findMedianShouldReturn6ForOddLengthArrayTest() {
+    public void findMedianShouldMedianValueForOddLengthArrayTest() {
         double[] arrayTest = {4, 1, 7, 6, 10};
 
         assertThat(Methods.findMedian(arrayTest), is(6.0));
     }
 
     @Test
-    public void findMedianShouldReturn6coma5ForEvenLengthArrayTest() {
+    public void findMedianShouldReturnMedianValueForEvenLengthArrayTest() {
         double[] arrayTest = {4, 1, 7, 6, 7, 10};
 
         assertThat(Methods.findMedian(arrayTest), is(6.5));
     }
 
     @Test
-    public void findModeShouldReturn2ForArrayTest() {
-        double[] arrayTest = {4,2,4,3,2,2};
+    public void findModeShouldReturnModeValueForArrayTest1() {
+        double[] arrayTest1 = {4,2,4,3,2,2};
 
-        assertThat(Methods.findMode(arrayTest), is(2.0));
+        assertThat(Methods.findMode(arrayTest1), is(2.0));
     }
 
     @Test
-    public void findStandardDeviationShouldReturn1coma8708ForArrayTest() {
-        double[] arrayTest = {6,2,3,1};
+    public void findStandardDeviationShouldReturnStandardDeviationValueForArrayTest1() {
+        double[] arrayTest1 = {6,2,3,1};
 
-        assertThat(Methods.findStandardDeviation(arrayTest), is(1.8708));
+        assertThat(Methods.findStandardDeviation(arrayTest1), is(1.8708));
     }
 
     @Test
-    public void findCoefficientOfVariationShouldReturn1coma8708ForArrayTest() {
-        double[] arrayTest = {6,2,3,1};
+    public void findCoefficientOfVariationShouldReturnCoefficientOfVariationValueForArrayTest1() {
+        double[] arrayTest1 = {6,2,3,1};
 
-        assertThat(Methods.findMean(arrayTest), is(3.0));
-        assertThat(Methods.findStandardDeviation(arrayTest), is(1.8708));
-        assertThat(Methods.findCoefficientOfVariation(arrayTest), is(62.36));
+        assertThat(Methods.findMean(arrayTest1), is(3.0));
+        assertThat(Methods.findStandardDeviation(arrayTest1), is(1.8708));
+        assertThat(Methods.findCoefficientOfVariation(arrayTest1), is(62.36));
     }
 
     @Test
@@ -84,6 +84,20 @@ public class MethodsTest {
         double[] values = {3,5,0,1,2,0,0,1};
 
         assertThat(Methods.findAmountOfDownwardSessions(values), is(2));
+    }
+
+    @Test
+    public void findAmountOfUnchangedSessionsShouldReturn2() {
+        double[] values = {5,4,6,6,9,4,3,3,1};
+
+        assertThat(Methods.findAmountOfUnchangedSessions(values), is(2));
+    }
+
+    @Test
+    public void findAmountOfUnchangedSessionsShouldReturnZero() {
+        double[] values = {5,4,6,9,4,3,1};
+
+        assertThat(Methods.findAmountOfUnchangedSessions(values), is(0));
     }
 
 }
